e39d109dd2a35315dd31f839fcc7e99ca55c4d81
Thanh_Vo <thanhvp2707@gmail.com>
2022-05-25 09:47:08 +0700

    config PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
+require('dotenv').config();
 const PORT = process.env.PORT || 8900
-const io = require("socket.io")(8900, {
+const io = require("socket.io")(PORT, {
   cors: {
     origin: "http://localhost:3000",
   },
